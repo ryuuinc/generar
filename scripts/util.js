b0f73dd932be0654f66f9aa198720da0d0defd91
Tienuon <ryuuinc@gmail.com>
2020-04-19 15:24:37 +0800

    fix: remove pathResolve

--- scripts/util.js
+++ scripts/util.js
@@ -3,9 +3,6 @@ const decodeBase64 = (base64Str) => {
   return new Buffer.from(base64Str, 'base64').toString();
 };
 
-/* path resolve */
-const pathResolve = (src) => path.resolve(__dirname, src);
-
 /* handle promise */
 const packPromise = (promise) => {
   return promise.then((result) => [undefined, result]).catch((error) => [error, undefined]);
