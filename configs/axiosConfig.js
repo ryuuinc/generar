26d91c9ed5eaf033a4f328a7c56b4a47e4b4e438
Tienuon <ryuuinc@gmail.com>
2023-02-22 16:50:10 +0800

    refactor: make generar more easier

--- configs/axiosConfig.js
+++ configs/axiosConfig.js
@@ -1,16 +1,16 @@
-const socksProxyAgent = require('socks-proxy-agent');
+const { SocksProxyAgent } = require('socks-proxy-agent');
 
 let axiosConfig = {
   headers: {
     'User-Agent':
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
   }
 };
 
-if (process.env.SOCKS_PROXY_AGENT != null) {
+if (process.env.PRODUCTION != 'true') {
   axiosConfig = Object.assign(axiosConfig, {
     proxy: false,
-    httpsAgent: new socksProxyAgent(process.env.SOCKS_PROXY_AGENT)
+    httpsAgent: new SocksProxyAgent(process.env.SOCKS_PROXY)
   });
 }
 
