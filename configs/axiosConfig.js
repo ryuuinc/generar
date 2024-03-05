5492723426555f9c3d9edbb17d18dfac9391a389
Tienuon <ryuuinc@gmail.com>
2024-03-05 22:38:52 +0800

    chore: remove semi and update

--- configs/axiosConfig.js
+++ configs/axiosConfig.js
@@ -1,17 +1,17 @@
-const { SocksProxyAgent } = require('socks-proxy-agent');
+const { SocksProxyAgent } = require('socks-proxy-agent')
 
 let axiosConfig = {
   headers: {
     'User-Agent':
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
   }
-};
+}
 
 if (process.env.PRODUCTION != 'true') {
   axiosConfig = Object.assign(axiosConfig, {
     proxy: false,
     httpsAgent: new SocksProxyAgent(process.env.SOCKS_PROXY)
-  });
+  })
 }
 
-module.exports = axiosConfig;
+module.exports = axiosConfig
